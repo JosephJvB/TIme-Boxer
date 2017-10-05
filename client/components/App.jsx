7668d72faf4572309d999b66edb06ba12c27086d
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-06 11:36:43 +1300

    split html elements

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -47,6 +47,26 @@ class App extends React.Component {
         {this.state.isStartVisible && <Start start={this.handleStart}/>}
         {this.state.isDuringVisible && <During stop={this.handleStop}/>}
         {this.state.isEndVisible && <End rematch={this.handleRematch} />}
+
+
+        <section className="hero">
+          <div className="hero-body" >
+            <div className="container">
+              <h1 className="title">
+              Welcome to the Time-Boxer!
+              </h1>
+              <h2 className="subtitle">
+                <br/> Can you make a coffee in under three minutes?
+              Can you write a Stateful component in under five minutes?
+                <br/>Time yourself - set your time and see if you can bet the clock.
+              </h2>
+            </div>
+          </div>
+        </section>
+        <figure className="image is-128x128" >
+          <img src="http://media.istockphoto.com/photos/alarm-clock-picture-id174764752?k=6&m=174764752&s=612x612&w=0&h=-LNUItbV31NAHsiT8cmXhqAbfQmuZ9bP9eB0ZSc0r14="/>
+        </figure>
+        <a className="button is-danger is-hovered">Click here</a>
       </div>
     )
   }
--- client/components/Start.jsx
+++ client/components/Start.jsx
@@ -5,6 +5,50 @@ const Start = (props) => {
     <div>
       <h1>This is start</h1>
       <button onClick={props.start}>Start!</button>
+
+      <div className="tile is-ancestor">
+        <div className="tile is-vertical is-8">
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child notification is-primary">
+                <p className="title">So you wanna Time yourself? See how fast you are?</p>
+                <p className="subtitle">This app is designed to to exactly that - see just how fast you can be </p>
+
+              </article>
+
+            </div>
+            <div className="tile is-parent">
+              <article className="tile is-child notification is-warning">
+                <p className="title">Tasks to Time</p>
+                <p className="subtitle">Pick your task - is it making coffee? is it working out? is it codeing? You can time anything</p>
+                <figure className="image is-4by4">
+                  <img src="https://marketplace.canva.com/MACRIqfAJf0/1/thumbnail_large/canva-angry-businessman-with-boxing-gloves--MACRIqfAJf0.jpg"/>
+                </figure>
+              </article>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-primary">
+              <p className="title">Wanna get Started?</p>
+              <p className="subtitle">Click Below to start your timer</p>
+              <div className="content">
+                <a className="button is-danger is-hovered">Click here</a>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-primary">
+            <div className="content">
+              <p className="title">Our History</p>
+              <p className="subtitle">Some of fasters coffee makers and coders have used this timer to help them get where they are today - and you can too </p>
+              <div className="content">
+
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
     </div>
   )
 }
