5f73908983ab1e6062af80f193aba42a4edc3446
Richard Johns <richard.johns193@gmail.com>
2017-10-06 14:35:04 +1300

    timer display

--- client/components/During.jsx
+++ client/components/During.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 class During extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {time: {m: 1, s: 60}, seconds: 300}
+    const startMins = 5
+    this.state = this.secondsToTime(startMins * 60)
     this.timer = 0
     this.startTimer = this.startTimer.bind(this)
     this.countDown = this.countDown.bind(this)
   }
+
   secondsToTime (secs) {
     let hours = Math.floor(secs / (60 * 60))
     let divisorForMinutes = secs % (60 * 60)
@@ -18,39 +20,38 @@ class During extends React.Component {
     let seconds = Math.ceil(divisorForSeconds)
 
     let obj = {
-      'h': hours,
-      'm': minutes,
-      's': seconds
+      'min': minutes,
+      'sec': seconds
     }
     return obj
   }
+
   componentDidMount () {
-    let timeLeftVar = this.secondsToTime(this.state.seconds)
-    this.setState({time: timeLeftVar})
+    this.startTimer()
   }
+
   startTimer () {
     if (this.timer === 0) {
       this.timer = setInterval(this.countDown, 1000)
     }
   }
+
   countDown () {
-    let seconds = this.state.seconds - 1
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds
-    })
+    let totalSecs = (this.state.min * 60) + this.state.sec - 1
+    this.setState(this.secondsToTime(totalSecs))
     // maybe is here to stop the timer..
-    if (seconds === 0) {
+    if (totalSecs === 0) {
       clearInterval(this.timer)
     }
   }
+
   render () {
     // const During = (props) => {
     return (
       <div>
         <h1>Clock is on!</h1>
-        <button onClick={this.props.stop}>Stop the timer!</button> {' '}
-          m: {this.state.time.m} s: {this.state.time.s}
+          m: {this.state.min} s: {this.state.sec} {' '}
+        <button onClick={this.props.stop}>Stop the timer!</button>
       </div>
     )
     // }
